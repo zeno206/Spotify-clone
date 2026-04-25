7c557425e7181d94167c698e70e7d1cda7b48a59
shubhamdhiman <shubham.dhiman.mehra@gmail.com>
2026-04-25 16:32:30 +0530

    main

--- spotify-backend/src/app.js
+++ spotify-backend/src/app.js
@@ -11,7 +11,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://spotify-clone-sepia-nu.vercel.app",
+    ],
     credentials: true,
   }),
 );
