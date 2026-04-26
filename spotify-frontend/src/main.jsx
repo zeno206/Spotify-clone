2d92bd1b4b826084c5677512a68d28221bd81031
shubhamdhiman <shubham.dhiman.mehra@gmail.com>
2026-04-26 12:16:08 +0530

    amin

--- spotify-frontend/src/main.jsx
+++ spotify-frontend/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 );
